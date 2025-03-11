15a03d65ba89cfb8d1d1b0869c8f53084ce68d06
AndisPerime <andisbudzilko@gmail.com>
2025-03-11 11:26:33 +0000

    initialize Django project structure with main_app and expenses_tracker

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Expense
+
+# Create your views here.
+def home(request):
+    expenses = Expense.objects.all()
+    return render(request, 'main_app/home.html', {'expenses': expenses})
