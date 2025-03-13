90e443c10322f2b3777c59efbba257321c78e59b
AndisPerime <andisbudzilko@gmail.com>
2025-03-13 11:20:21 +0000

    add: enhance Expense dashboard with summary cards, recent expenses table, responsive design; upd Expense model date input format

--- main_app/models.py
+++ main_app/models.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Added to Budget"))
 
 # Create your models here.
+
+
 class Expense(models.Model):
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField()
+    date = models.DateField(input_formats='%d/%m/%Y')
     category = models.CharField(max_length=50)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='expenses_inputs'
