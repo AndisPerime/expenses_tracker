0610ac3306616715c3d6215ea638a25c28291db6
AndisPerime <andisbudzilko@gmail.com>
2025-03-13 13:27:14 +0000

    refactor: update ExpenseForm fields formatting and remove unused import in views

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Expense
 
+
 class ExpenseForm(forms.ModelForm):
     date = forms.DateField(input_formats=["%d %b %Y"])
 
     class Meta:
         model = Expense
-        fields = ['name', 'amount', 'date', 'category', 'author', 'content', 'status']
+        fields = [
+            'name', 'amount', 'date', 'category',
+            'author', 'content', 'status'
+            ]
