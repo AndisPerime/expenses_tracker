1be15fe809fb93f990546e46d6bcec8133ac95f7
AndisPerime <andisbudzilko@gmail.com>
2025-03-18 12:33:58 +0000

    debug: false

--- expenses_tracker/settings.py
+++ expenses_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = (
     )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Set to True for development to serve static files
+DEBUG = False  # Set to True for development to serve static files
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
