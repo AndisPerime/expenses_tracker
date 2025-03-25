75d35feafffd29cd454666e5ef3425261743c61e
AndisPerime <andisbudzilko@gmail.com>
2025-03-25 13:23:11 +0000

    removing of whitespaces

--- main_app/forms.py
+++ main_app/forms.py
@@ -3,6 +3,7 @@ from .models import Expense
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
+
 class ExpenseForm(forms.ModelForm):
     date = forms.DateField(input_formats=["%d %b %Y"])
 
@@ -11,20 +12,22 @@ class ExpenseForm(forms.ModelForm):
         fields = [
             'name', 'amount', 'date', 'category',
             'author', 'content', 'status'
-            ]
+        ]
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Add Bootstrap classes to form fields
         for field_name in self.fields:
             self.fields[field_name].widget.attrs['class'] = 'form-control'
+
 
 class CustomAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
